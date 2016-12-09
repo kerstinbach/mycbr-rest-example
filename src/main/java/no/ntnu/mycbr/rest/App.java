87fab42828aecfa5d18c91d83b2773671ab379bc
Truls Mørk Pettersen <trulsmp@stud.ntnu.no>
2016-12-09 15:47:32 +0100

    works

--- src/main/java/no/ntnu/mycbr/rest/App.java
+++ src/main/java/no/ntnu/mycbr/rest/App.java
@@ -1,9 +1,15 @@
 package no.ntnu.mycbr.rest;
 
 import de.dfki.mycbr.core.Project;
+import de.dfki.mycbr.core.casebase.Instance;
+import de.dfki.mycbr.core.model.AttributeDesc;
+import de.dfki.mycbr.core.model.Concept;
+import de.dfki.mycbr.io.CSVImporter;
 import no.ntnu.mycbr.CBREngine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collection;
 
 /**
  * Hello world!
@@ -19,10 +25,42 @@ public class App {
     private static CBREngine engine;
     private static Project project;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
 
         engine = new CBREngine();
         project = engine.createProjectFromPRJ();
+
+
+        Concept myconcept = project.getConceptByID("Trip");
+
+
+        Collection<Instance> cases = project.getAllInstances();
+
+        // removes all cases currently in the case_base
+        for (Instance instance : cases) {
+            project.removeCase(instance.getName());
+        }
+
+
+        // Imports the cases to the case_case
+        CSVImporter csvImporter = new CSVImporter(System.getProperty("user.dir") + "/src/main/resources/cases.csv",myconcept);
+        csvImporter.setSeparator(";");
+        csvImporter.setSeparatorMultiple("!");
+
+
+        csvImporter.readData();
+        csvImporter.checkData();
+        csvImporter.setCaseBase(myconcept.getProject().getCB("main_case_base"));
+        csvImporter.addMissingDescriptions();
+        csvImporter.addMissingValues();
+        csvImporter.doImport();
+
+
+
+
+
+
         SpringApplication.run(App.class, args);
     }
 
